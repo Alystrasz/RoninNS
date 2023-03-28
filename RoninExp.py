33ff35ed871dadf612c5b9e04598a8656681f531
Command-Git <105081942+Command-Git@users.noreply.github.com>
2023-03-28 11:19:17 -0500

    Update

--- RoninExp.py
+++ RoninExp.py
@@ -13,5 +13,17 @@ def grabInstall():
     print(f"Grabbing NS v{version}...")
     os.system("cd ~; mkdir .ronintemp; cd .ronintemp; curl https://www.github.com/R2Northstar/Northstar/v{version}/Northstar.release.v{version}.zip; unzip Northstar.release.v{version}.zip; cp -a Northstar.release.v{version}. ~/.local/share/Steam/steamapps/common/Titanfall2; cd ~; rm -r .ronintemp")
     print("Installed Northstar!")
+def grab():
+  print("Grab Tool - Select a version (L for latest)")
+  version = input("Version: ")
+  if version == 'L':
+    print("Grabbing latest. It will be saved to Downloads.")
+    os.system(f"cd ~/Downloads; curl https://www.github.com/R2Northstar/Northstar/v{latest}/Northstar.release.v{latest}.zip")
+    print("Grab successful!")
+  else:
+    print("Grabbing NS v{latest}...")
+    os.system(f"cd ~/Downloads; curl https://www.github.com/R2Northstar/Northstar/v{version}/Northstar.release.v{version}.zip")
+    print("Grab successful!")
 print("Ronin - The CLI for Northstar - v1.2.0")
 print("What would you like to do?")
+print("1. Grab and Install\n2. Grab\n0. Exit")
